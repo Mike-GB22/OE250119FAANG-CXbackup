978d730aeaa93a7135065e989d21568d71ddc748
alexey_obraztsov <alexey.autotester86@gmail.com>
2025-02-15 10:24:16 +0300

    BJS-52586 Recommendations. unused imports deleted

--- src/main/java/school/faang/user_service/service/RecommendationService.java
+++ src/main/java/school/faang/user_service/service/RecommendationService.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.dto.recommendation.RecommendationDto;
