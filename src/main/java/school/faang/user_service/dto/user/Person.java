6ce4d925f882a3b326bdd49e8ed85d91e5cfc38b
Mipusatina <mipspam@mail.ru>
2025-03-06 02:14:21 +0100

    Person DTO

--- src/main/java/school/faang/user_service/dto/user/Person.java
+++ src/main/java/school/faang/user_service/dto/user/Person.java
@@ -1,11 +1,50 @@
 package school.faang.user_service.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import lombok.Data;
 
-public record Person (
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Person {
         @JsonProperty("firstName")
-        String firstName,
+        private String firstName;
 
         @JsonProperty("lastName")
-        String lastName
-){}
+        private String lastName;
+
+        @JsonUnwrapped
+        ContactInfo contactInfo;
+
+        @JsonUnwrapped
+        Education education;
+
+        String employer;
+
+        @Data
+        public static class ContactInfo {
+             String email;
+             String phone;
+
+             @JsonUnwrapped
+             Address address;
+
+             @Data
+             public static class Address {
+                     String street;
+                     String city;
+                     String state;
+                     String country;
+                     String postalCode;
+             }
+        }
+
+        @Data
+        public static class Education {
+                String faculty;
+                String yearsOfStudy;
+                String major;
+                String GPA;
+        }
+}
