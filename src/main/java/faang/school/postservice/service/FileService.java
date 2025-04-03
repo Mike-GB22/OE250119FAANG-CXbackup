e44f2a2d2bc92ac2dfecc0d19dfa6192d9fa93cd
Mipusatina <mipspam@mail.ru>
2025-04-03 12:19:25 +0200

    FileService: Resize images to 2 Thumbnail 1080 and 170px

--- src/main/java/faang/school/postservice/service/FileService.java
+++ src/main/java/faang/school/postservice/service/FileService.java
@@ -1,6 +1,5 @@
 package faang.school.postservice.service;
 
-import com.fasterxml.jackson.datatype.jdk8.WrappedIOException;
 import faang.school.postservice.config.context.UserContext;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -29,7 +28,8 @@ public class FileService {
     private static final String SMALL_IMAGE_KEY_PATTERN = "small/%s";
 
     private static final List<String> IMAGE_EXTENSIONS = List.of(".jpg", ".jpeg", ".png", ".gif", ".bmp", ".wbmp");
-    private static final int WIDTH_OF_IMAGE_PX = 300;
+    private static final int MAX_SIDE_SIZE_OF_SMALL_IMAGE_PX = 170;
+    private static final int MAX_SIDE_SIZE_OF_IMAGE_PX = 1080;
 
     private final S3Client s3Client;
     private final UserContext userContext;
@@ -41,14 +41,13 @@ public class FileService {
 
     @SneakyThrows
     public String uploadFile(MultipartFile file)  {
-        String fileKey = getFileKey(file);
-
-        putObjectInS3Client(file, fileKey);
+        String fileKey;
 
         if (isFileAnImage(file)) {
-            putObjectInS3Client(
-                    makeThumbnail(file)
-                    , String.format(SMALL_IMAGE_KEY_PATTERN, fileKey));
+            fileKey = processingImageFile(file);
+        } else {
+            fileKey = createFileKey(file.getOriginalFilename());
+            putObjectInS3Client(file, fileKey);
         }
 
         return fileKey;
@@ -56,28 +55,59 @@ public class FileService {
 
     public InputStream downloadFile(String fileKey) {
         return s3Client.getObject(
-                getGetObjectResponse(fileKey));
+                buildGetObjectResponse(fileKey));
+    }
+
+    @SneakyThrows
+    private String processingImageFile(MultipartFile file) {
+        String fileKey = createFileKey(file.getOriginalFilename() + ".jpg");
+
+        InputStream inputStream = file.getInputStream();
+        BufferedImage bufferedImage = ImageIO.read(inputStream);
+
+        putObjectInS3Client(
+                makeThumbnailMultipartFile(bufferedImage, fileKey, MAX_SIDE_SIZE_OF_IMAGE_PX)
+                , fileKey);
+
+        putObjectInS3Client(
+                makeThumbnailMultipartFile(bufferedImage, fileKey, MAX_SIDE_SIZE_OF_SMALL_IMAGE_PX)
+                , String.format(SMALL_IMAGE_KEY_PATTERN, fileKey));
+
+        return fileKey;
     }
 
 
     @SneakyThrows
-    private MultipartFile makeThumbnail(MultipartFile file) {
-        InputStream inputStream = file.getInputStream();
+    private MultipartFile makeThumbnailMultipartFile(BufferedImage bufferedImage, String fileName, int maxSize) {
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        BufferedImage bufferedImage = ImageIO.read(inputStream);
+        int[] newWidthHeight = calculateNewWidthHeight(bufferedImage.getWidth(), bufferedImage.getHeight(), maxSize);
+
         Thumbnails.of(bufferedImage)
-                .size(WIDTH_OF_IMAGE_PX, getNewHeightOfImage(bufferedImage, WIDTH_OF_IMAGE_PX))
+                .size(newWidthHeight[0], newWidthHeight[1])
                 .outputFormat("jpg")
                 .toOutputStream(outputStream);
 
-        String fileName = file.getOriginalFilename()+".jpg";
         String contentType = "image/jpeg";
-        return new MyMultipartFile(outputStream.toByteArray(), file.getName(), fileName, contentType);
+        String name = "resized image";
+        return new MyMultipartFile(outputStream.toByteArray(), name, fileName, contentType);
     }
 
-    private int getNewHeightOfImage(BufferedImage bufferedImage, double newWidth) {
-        double newHeight = bufferedImage.getHeight() * newWidth / bufferedImage.getWidth();
-        return (int) Math.ceil(newHeight);
+    private int[] calculateNewWidthHeight(int width, int height, int maxSize) {
+        if (width <= maxSize || height <= maxSize) {
+            return new int[]{width, height};
+        }
+
+        int newWidth;
+        int newHeight;
+        if (width >= height) {
+            newWidth = maxSize;
+            newHeight = (int) Math.ceil(((double) newWidth * height) / width);
+        } else {
+            newHeight = maxSize;
+            newWidth = (int) Math.ceil(((double) newHeight * width) / height);
+        }
+
+        return new int[]{newWidth, newHeight};
     }
 
     private boolean isFileAnImage(MultipartFile file) {
@@ -91,16 +121,15 @@ public class FileService {
         return false;
     }
 
+
+    @SneakyThrows
     private void putObjectInS3Client(MultipartFile file, String fileKey) {
-        try {
-            s3Client.putObject(
-                    getPutObjectRequest(file, fileKey), getRequestBody(file));
-        } catch (IOException e) {
-            throw new WrappedIOException(e);
-        }
+        s3Client.putObject(
+                buildPutObjectRequest(file, fileKey)
+                , buildRequestBody(file));
     }
 
-    private PutObjectRequest getPutObjectRequest(MultipartFile file, String fileKey) {
+    private PutObjectRequest buildPutObjectRequest(MultipartFile file, String fileKey) {
         return PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(fileKey)
@@ -108,21 +137,21 @@ public class FileService {
                 .build();
     }
 
-    private RequestBody getRequestBody(MultipartFile file) throws IOException {
+    private RequestBody buildRequestBody(MultipartFile file) throws IOException {
         return RequestBody.fromInputStream(file.getInputStream(), file.getSize());
     }
 
-    private GetObjectRequest getGetObjectResponse(String fileKey) {
+    private GetObjectRequest buildGetObjectResponse(String fileKey) {
         return GetObjectRequest.builder()
                 .bucket(bucketName)
                 .key(fileKey)
                 .build();
     }
 
-    private String getFileKey(MultipartFile file) {
+    private String createFileKey(String fileName) {
         String pathName = DEFAULT_USER_ID;
         String timeStamp = String.valueOf(System.nanoTime());
 
-        return String.format(FILE_KEY_PATTERN, pathName, timeStamp, file.getOriginalFilename());
+        return String.format(FILE_KEY_PATTERN, pathName, timeStamp, fileName);
     }
 }
